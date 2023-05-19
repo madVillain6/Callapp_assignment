90784ad287534a6d4b8f78a6510dc17b4bc0cdbf
madVillain6 <levanbichinashvili5@gmai.com>
2023-05-19 22:26:02 +0400

    fix: path to DATA.json

--- server/index.js
+++ server/index.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
+const FILE_PATH = path.join(__dirname, "DATA.json");
 
 const app = express();
 
 const port = 3000;
 
+const dataMiddleware = (req, res, next) => {
+  fs.readFile("DATA.json", "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading data");
+    } else {
+      req.fileData = JSON.parse(data);
+      next();
+    }
+  });
+};
+app.use(dataMiddleware());
 app.use(express.json());
 app.use(cors());
 
 //GET method
 
 app.get("/api/data", (req, res) => {
-  fs.readFile("DATA.json", "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading data");
-    } else {
-      res.json(JSON.parse(data));
-    }
-  });
+  res.json(JSON.parse(req.fileData));
 });
 
 //POST method
@@ -27,22 +34,15 @@ app.get("/api/data", (req, res) => {
 app.post("/api/data", (req, res) => {
   const newData = req.body;
 
-  fs.readFile("DATA.json", "utf-8", (err, data) => {
+  const dataArray = JSON.parse(data);
+  dataArray.push(newData);
+
+  fs.writeFile(FILE_PATH, JSON.stringify(dataArray), "utf-8", (err) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Error reading data");
+      res.status(500).send("Error writing Data");
     } else {
-      const dataArray = JSON.parse(data);
-      dataArray.push(newData);
-
-      fs.writeFile("DATA.json", JSON.stringify(dataArray), "utf-8", (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error writing Data");
-        } else {
-          res.send("data added successfully");
-        }
-      });
+      res.send("data added successfully");
     }
   });
 });
@@ -54,58 +54,44 @@ app.put("/api/data/:id", (req, res) => {
 
   const updateData = req.body;
 
-  fs.readFile("DATA.json", "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading data");
-    } else {
-      const dataArray = JSON.parse(data);
-      const dataIndex = dataArray.findIndex((item) => item.id === id);
+  const dataArray = JSON.parse(data);
+  const dataIndex = dataArray.findIndex((item) => item.id === id);
 
-      if (dataIndex === -1) {
-        res.status(404).send("Data not found");
+  if (dataIndex === -1) {
+    res.status(404).send("Data not found");
+  } else {
+    dataArray[dataIndex] = updateData;
+
+    fs.writeFile(FILE_PATH, JSON.stringify(dataArray), "utf-8", (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error writing data");
       } else {
-        dataArray[dataIndex] = updateData;
-
-        fs.writeFile("DATA.json", JSON.stringify(dataArray), "utf-8", (err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send("Error writing data");
-          } else {
-            res.send("Data updated successfully");
-          }
-        });
+        res.send("Data updated successfully");
       }
-    }
-  });
+    });
+  }
 });
 
 app.delete("/api/data/:id", (req, res) => {
   const id = Number(req.params.id);
 
-  fs.readFile("DATA.json", "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading data");
-    } else {
-      const dataArray = JSON.parse(data);
-      const dataIndex = dataArray.findIndex((item) => item.id === id);
-      if (dataIndex === -1) {
-        res.status(404).send("Data not found");
-      } else {
-        dataArray.splice(dataIndex, 1);
+  const dataArray = JSON.parse(data);
+  const dataIndex = dataArray.findIndex((item) => item.id === id);
+  if (dataIndex === -1) {
+    res.status(404).send("Data not found");
+  } else {
+    dataArray.splice(dataIndex, 1);
 
-        fs.writeFile("DATA.json", JSON.stringify(dataArray), "utf-8", (err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send("Error writing data");
-          } else {
-            res.send("Data deleted successfully");
-          }
-        });
+    fs.writeFile(FILE_PATH, JSON.stringify(dataArray), "utf-8", (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error writing data");
+      } else {
+        res.send("Data deleted successfully");
       }
-    }
-  });
+    });
+  }
 });
 
 app.listen(port, () => {
